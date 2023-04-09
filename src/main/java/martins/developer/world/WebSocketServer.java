6708001e132aa9e4198e083153957ecd5b2c1e0c
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-04-09 17:17:47 -0400

    Get current versions of all the wonderfulness.

--- src/main/java/martins/developer/world/WebSocketServer.java
+++ src/main/java/martins/developer/world/WebSocketServer.java
@@ -19,61 +19,61 @@ import java.util.List;
 import static io.undertow.Handlers.*;
 
 public class WebSocketServer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketServer.class);
-    private Undertow server;
-    String lastReceivedMessage;
+  private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+  private Undertow server;
+  String lastReceivedMessage;
 
-    public static void main(final String[] args) {
-        WebSocketServer webSocketServer = new WebSocketServer();
-        webSocketServer.buildAndStartServer(8080, "localhost");
+  public static void main(final String[] args) {
+    WebSocketServer webSocketServer = new WebSocketServer();
+    webSocketServer.buildAndStartServer(8080, "localhost");
+  }
+
+  public void buildAndStartServer(int port, String host) {
+    server = Undertow.builder()
+      .addHttpListener(port, host)
+      .setHandler(getWebSocketHandler())
+      .setWorkerThreads(1)
+      .build();
+    server.start();
+    while (true) {
+      try {
+        Thread.sleep(1000);
+        for (var wsChannel : wsChannels) {
+          WebSockets.sendText(new Date() + " " + wsChannel.toString(), wsChannel, null);
+        }
+      } catch (InterruptedException e) {
+        LOGGER.info("oops", e);
+      }
     }
+  }
 
-    public void buildAndStartServer(int port, String host) {
-        server = Undertow.builder()
-                .addListener(port, host)
-                .setHandler(getWebSocketHandler())
-                .setWorkerThreads(1)
-                .build();
-        server.start();
-        while (true) {
-            try {
-                Thread.sleep(1000);
-                for (var wsChannel : wsChannels) {
-                    WebSockets.sendText(new Date() + " "  + wsChannel.toString(), wsChannel, null);
-                }
-            } catch (InterruptedException e) {
-                LOGGER.info("oops", e);
+  List<WebSocketChannel> wsChannels = new ArrayList<>();
+
+  public void stopServer() {
+    if (server != null) {
+      server.stop();
+    }
+  }
+
+  private PathHandler getWebSocketHandler() {
+    return path()
+      .addPrefixPath("/websocket", websocket(new WebSocketConnectionCallback() {
+        @Override
+        public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+          channel.getReceiveSetter().set(new AbstractReceiveListener() {
+            @Override
+            protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
+              String data = message.getData();
+              lastReceivedMessage = data;
+              LOGGER.info("Received data: " + data);
+              WebSockets.sendText(data, channel, null);
             }
+          });
+          channel.resumeReceives();
+          wsChannels.add(channel);
         }
-    }
-    List<WebSocketChannel> wsChannels = new ArrayList<>();
-
-    public void stopServer() {
-        if (server != null) {
-            server.stop();
-        }
-    }
-
-
-    private PathHandler getWebSocketHandler() {
-        return path()
-            .addPath("/websocket", websocket(new WebSocketConnectionCallback() {
-                @Override
-                public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
-                    channel.getReceiveSetter().set(new AbstractReceiveListener() {
-                        @Override
-                        protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
-                            String data = message.getData();
-                            lastReceivedMessage = data;
-                            LOGGER.info("Received data: " + data);
-                            WebSockets.sendText(data, channel, null);
-                        }
-                    });
-                    channel.resumeReceives();
-                    wsChannels.add(channel);
-                }
-            }))
-            .addPath("/", resource(new ClassPathResourceManager(WebSocketServer.class.getClassLoader(), WebSocketServer.class.getPackage()))
-                    .addWelcomeFiles("index.html"));
-    }
+      }))
+      .addPrefixPath("/", resource(new ClassPathResourceManager(WebSocketServer.class.getClassLoader(), WebSocketServer.class.getPackage()))
+        .addWelcomeFiles("index.html"));
+  }
 }
