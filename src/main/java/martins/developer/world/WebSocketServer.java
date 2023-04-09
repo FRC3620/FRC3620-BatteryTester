05c72e609fe9169ba0308981ea317c8a148cc2f2
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-04-09 16:57:19 -0400

    Get current versions of all the wonderfulness.

--- src/main/java/martins/developer/world/WebSocketServer.java
+++ src/main/java/martins/developer/world/WebSocketServer.java
@@ -7,7 +7,7 @@ import io.undertow.websockets.core.AbstractReceiveListener;
 import io.undertow.websockets.core.BufferedTextMessage;
 import io.undertow.websockets.core.WebSocketChannel;
 import io.undertow.websockets.core.WebSockets;
-import io.undertow.websockets.core.handler.WebSocketConnectionCallback;
+import io.undertow.websockets.WebSocketConnectionCallback;
 import io.undertow.websockets.spi.WebSocketHttpExchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,23 +56,24 @@ public class WebSocketServer {
 
 
     private PathHandler getWebSocketHandler() {
-        return path().addPath("/websocket", websocket(new WebSocketConnectionCallback() {
-            @Override
-            public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
-                channel.getReceiveSetter().set(new AbstractReceiveListener() {
-                    @Override
-                    protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
-                        String data = message.getData();
-                        lastReceivedMessage = data;
-                        LOGGER.info("Received data: " + data);
-                        WebSockets.sendText(data, channel, null);
-                    }
-                });
-                channel.resumeReceives();
-                wsChannels.add(channel);
-            }
-        }))
-                .addPath("/", resource(new ClassPathResourceManager(WebSocketServer.class.getClassLoader(), WebSocketServer.class.getPackage()))
-                        .addWelcomeFiles("index.html"));
+        return path()
+            .addPath("/websocket", websocket(new WebSocketConnectionCallback() {
+                @Override
+                public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+                    channel.getReceiveSetter().set(new AbstractReceiveListener() {
+                        @Override
+                        protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
+                            String data = message.getData();
+                            lastReceivedMessage = data;
+                            LOGGER.info("Received data: " + data);
+                            WebSockets.sendText(data, channel, null);
+                        }
+                    });
+                    channel.resumeReceives();
+                    wsChannels.add(channel);
+                }
+            }))
+            .addPath("/", resource(new ClassPathResourceManager(WebSocketServer.class.getClassLoader(), WebSocketServer.class.getPackage()))
+                    .addWelcomeFiles("index.html"));
     }
 }
