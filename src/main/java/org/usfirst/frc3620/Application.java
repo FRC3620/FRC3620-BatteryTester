6234801f80ec2c5039a8d6a21273a66101e3399c
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-04-10 14:55:09 -0400

    Breaking routing and ws apart.

--- src/main/java/org/usfirst/frc3620/Application.java
+++ src/main/java/org/usfirst/frc3620/Application.java
@@ -1,14 +1,17 @@
 package org.usfirst.frc3620;
 
 import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.websockets.WebSocketConnectionCallback;
 import io.undertow.websockets.core.AbstractReceiveListener;
 import io.undertow.websockets.core.BufferedTextMessage;
 import io.undertow.websockets.core.WebSocketChannel;
 import io.undertow.websockets.core.WebSockets;
-import io.undertow.websockets.WebSocketConnectionCallback;
 import io.undertow.websockets.spi.WebSocketHttpExchange;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,17 +24,26 @@ import static io.undertow.Handlers.*;
 public class Application {
   private final Logger LOGGER = LoggerFactory.getLogger(getClass());
   private Undertow server;
-  String lastReceivedMessage;
 
   public static void main(final String[] args) {
-    Application webSocketServer = new Application();
-    webSocketServer.buildAndStartServer(8080, "localhost");
+    Application application = new Application();
+    application.buildAndStartServer(8080, "localhost");
   }
+
+  HttpHandler ROUTES = new RoutingHandler()
+          .get("/websocket", websocket(new WSTest()))
+          .get("/", resource(new ClassPathResourceManager(getClass().getClassLoader(), getClass().getPackage()))
+                  .addWelcomeFiles("index.html"));
+
+  PathHandler PATHS = path()
+            .addPrefixPath("/websocket", websocket(new WSTest()))
+            .addPrefixPath("/", resource(new ClassPathResourceManager(getClass().getClassLoader(), getClass().getPackage()))
+                    .addWelcomeFiles("index.html"));
 
   public void buildAndStartServer(int port, String host) {
     server = Undertow.builder()
       .addHttpListener(port, host)
-      .setHandler(getWebSocketHandler())
+      .setHandler(PATHS)
       .setWorkerThreads(1)
       .build();
     server.start();
@@ -47,33 +59,29 @@ public class Application {
     }
   }
 
+  class WSTest implements WebSocketConnectionCallback {
+    @Override
+    public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+      LOGGER.info("Got connection {}", channel);
+      channel.getReceiveSetter().set(new AbstractReceiveListener() {
+        @Override
+        protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
+          String data = message.getData();
+          LOGGER.info("Received data: {}", data);
+          WebSockets.sendText(data, channel, null);
+        }
+      });
+      channel.resumeReceives();
+      wsChannels.add(channel);
+    }
+
+  }
+
   List<WebSocketChannel> wsChannels = new ArrayList<>();
 
   public void stopServer() {
     if (server != null) {
       server.stop();
     }
-  }
-
-  private PathHandler getWebSocketHandler() {
-    return path()
-      .addPrefixPath("/websocket", websocket(new WebSocketConnectionCallback() {
-        @Override
-        public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
-          channel.getReceiveSetter().set(new AbstractReceiveListener() {
-            @Override
-            protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
-              String data = message.getData();
-              lastReceivedMessage = data;
-              LOGGER.info("Received data: " + data);
-              WebSockets.sendText(data, channel, null);
-            }
-          });
-          channel.resumeReceives();
-          wsChannels.add(channel);
-        }
-      }))
-      .addPrefixPath("/", resource(new ClassPathResourceManager(Application.class.getClassLoader(), Application.class.getPackage()))
-        .addWelcomeFiles("index.html"));
   }
 }
